865e375a2b8bda9e6f5173b63621d9901c671464
robson1622 <robson1622@gmail.com>
2023-06-11 20:37:53 -0300

    Atualizacao: CRUD e Conector funcionando parcialmente

--- src/CRUD.py
+++ src/CRUD.py
@@ -5,13 +5,46 @@ class CRUD:
         self.nomesTabelas = None
         self.numeroTabelas = 0
 
-    def CREATETABLE(self,nomeTabela,elementos):
-        try:
-            with open(nomeTabela,"w") as arquivo:
-                escritor = csv.writer(arquivo)
-                escritor.writerows(elementos)
-                
-            self.nomesTabelas[self.numeroTabelas] = nomeTabela
-            self.numeroTabelas += 1
-        except:
-            print("Não foi possível criar o arquivo CSV.")
+    #não retorna nada, apenas carrega na memória os arquivos do banco escolhido
+    #def carregarBanco(self,nomeBanco):
+
+    #retorna todos os bancos salvos
+    #def getDataBases(self):
+
+    #O arquivo que será criado tem como nome o banco de dados, e guarda na primeira coluna os nomes das tabelas
+    #que foram salvas em .csv, nas colunas subsequentes estão as colunas da tabela
+    #university.csv (nome do arquivo)
+    #instructor |   id   | dept_name | ...
+    #strudent   |   id   | name      | ...
+    #...
+    #
+    #Como controle geral, existe um txt com nome bancos.txt que salva os nomes dos bancos salvos.
+    def CREATEDATABASEFILE(self,nomeDB,linhas):
+        with open(nomeDB + ".csv", 'w', newline='') as arquivo:
+            writer = csv.writer(arquivo)
+            #writer.writerow(['Nome', 'Idade', 'Cidade'])  # Escreve o cabeçalho das colunas
+            for linha in linhas:
+                writer.writerow(linha)
+
+    #As tabelas serão salvas como instructor.csv, onde suas colunas são as colunas das tabelas.
+    def CREATETABLE(self,nomeTabela, colunas):
+        with open(nomeTabela, 'w', newline='') as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(colunas)
+
+    #teste
+    def PRINTCSV(self,nomeTabela):
+        with open(nomeTabela + ".csv", 'r') as arquivo:
+            reader = csv.reader(arquivo)
+            for linha in reader:
+                print(linha)
+    #def INSERT(self,nomeTabela,elemento):
+        
+
+cr = CRUD()
+nomeBanco = "university"
+linhas = [['instructor','id','dept_name'],
+          ['student','id','name'],
+          ['department','id','budget','title']]
+#cr.CREATEDATABASEFILE(nomeBanco,linhas)
+cr.PRINTCSV(nomeBanco)
--- src/ConectorMySQL.py
+++ src/ConectorMySQL.py
@@ -1,4 +1,14 @@
 
+#comandos em git
+#mostra o status dos arquivos, se foram upados ou não
+#git status
+# adciciona todos arquivos a fila de push
+#git add .
+#comentário para fazer o push
+#git commit -m "comentarios"
+#push sobe todos os arquivos
+#git push
+
 
 import mysql.connector
 
@@ -10,8 +20,21 @@ class MySQLConnection:
         self.database = database
         self.connection = None
 
+    def getConexao(self):
+        return self.connection
+    
+    #Quando for pegar a tabela, faça algo como nomeTabelaa =  tabelas[6][0]
+    #pois o retorno dos nomes das tabelas é uma tupla.
+    def getTabela(self,nomeTabela):
+        if self.connection:
+            cursor = self.connection.cursor()
+            cursor.execute("SELECT * FROM " + nomeTabela + ";")
+            return cursor.fetchall()
+        else :
+            print("Nenhuma conexão encontrada, não foi possível carregar a tabela.")
+
     #retorna verdadeiro se a conexão foi realizada
-    def conectado(self) -> bool:
+    def conectar(self) -> bool:
         try:
             self.connection = mysql.connector.connect(
                 host=self.host,
@@ -30,8 +53,8 @@ class MySQLConnection:
             print("Fim da conexão.")
 
     #retorna todas as tabelas existentes no banco de dados conectado
-    def getTabelas(self):
-        if self.conectado():
+    def getTodasTabelas(self):
+        if self.connection:
             cursor = self.connection.cursor()
             cursor.execute("SHOW TABLES")
             return cursor.fetchall()
@@ -41,24 +64,29 @@ class MySQLConnection:
         
     #tabelas selecionadas deve ser um vetor de True e False, que indica se deve-se importar aquela tabela
     def setTabelasImportar(self,tabelasSelecionadas):
-        tabelas = self.getTabelas()
+        tabelas = self.getTodasTabelas()
         i = 0
         for tabela in tabelas:
             if(tabelasSelecionadas[i]):
                 self.importarTabelaCSV(tabela)
             i+=1
 
-
+            
     def importarTabelaCSV(self,nomeTabela):
         novaTabela = open(nomeTabela,"w")
 
 
 # Exemplo de uso:
-connection = MySQLConnection('localhost', 'root', '033002970', 'employees')
-
-tabelas = connection.getTabelas()
-
-for tabela in tabelas:
-    print(tabela)
-
-connection.desconectar()
+#connection = MySQLConnection('localhost', 'root', '033002970', 'employees')
+#connection.conectar()
+#
+#tabelas = connection.getTodasTabelas()
+#
+#nomeTabelaa =  tabelas[6][0]
+#print(nomeTabelaa)
+#salaries = connection.getTabela(nomeTabelaa)
+#
+#for salario in salaries:
+#    print(salario)
+#
+#connection.desconectar()
